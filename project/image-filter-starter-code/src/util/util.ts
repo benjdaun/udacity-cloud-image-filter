a4d7392fbcbb983807dc89ce8c769c4e3e4feb38
benjdaun <benjamindaunoravicius@gmail.com>
2020-11-30 20:38:11 -0500

    Fixing endpoint to delete tmp files

--- project/image-filter-starter-code/src/util/util.ts
+++ project/image-filter-starter-code/src/util/util.ts
@@ -8,17 +8,17 @@ import Jimp = require('jimp');
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+    return new Promise(async resolve => {
         const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
+        const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
         await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
+            .resize(256, 256) // resize
+            .quality(60) // set JPEG quality
+            .greyscale() // set greyscale
+            .write(__dirname + outpath, (img) => {
+                resolve(__dirname + outpath);
+            });
     });
 }
 
@@ -27,8 +27,24 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
-        fs.unlinkSync(file);
-    }
+
+// function modified because it kept throwing errors
+export async function deleteLocalFiles(localDirectory: string) {
+
+    //First us the fs functionality to read list of files
+    fs.readdir(localDirectory, (err, files) => {
+        if (err)
+            console.log(err);
+        else {
+
+    //delete files from directory
+            files.forEach(file => {
+                fs.unlink(localDirectory + file, err => {
+                    if (err) throw err;
+                });
+            })
+
+        }
+    })
+
 }
